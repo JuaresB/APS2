73e8807a800e4f3aa9595e0c3cb2c0be27be54c5
Juares B <juares@consultorio.com>
2018-11-22 13:17:28 -0200

    implements maps with expo

--- App.js
+++ App.js
@@ -3,7 +3,8 @@ import { Alert, StyleSheet, Text, View, Button } from 'react-native';
 import Login from './components/login'
 import Signup from './components/signup'
 import Navigation from './components/navigation'
- 
+import DestinantionChoice from './components/destinationChoice'
+
 export default class App extends React.Component {
   constructor(props){
     super(props)
@@ -13,6 +14,7 @@ export default class App extends React.Component {
     this.onSignup = this.onSignup.bind(this)
     this.onSignIn = this.onSignIn.bind(this)
     this.onSendSignupForm = this.onSendSignupForm.bind(this)
+    this.onLocationChoice = this.onLocationChoice.bind(this)
   }
   onSignup() {
     this.setState({
@@ -21,38 +23,46 @@ export default class App extends React.Component {
   }
   onSignIn() {
     this.setState({
-      tela: "navigation" 
+      tela: "destinationChoice"
     });
   }
   onSendSignupForm() {
     this.setState({
-      tela: "signin"  
+      tela: "signin"
     });
   }
-  
-  render() { 
+  onLocationChoice(){
+    this.setState({
+      tela: "navigation"
+    })
+  }
+
+  render() {
     return (
       <View style={styles.container}>
         {this.state.tela === "signin" &&
-          <Login onSignIn={this.onSignIn} onSignup={this.onSignup}/> 
+          <Login onSignIn={this.onSignIn} onSignup={this.onSignup}/>
         }
         {this.state.tela === "signup" &&
           <Signup onSignup={this.onSendSignupForm}/>
-        } 
+        }
+        {this.state.tela === "destinationChoice" &&
+          <DestinantionChoice onLocationChoice={this.onLocationChoice}/>
+        }
         {this.state.tela === "navigation" &&
           <Navigation />
-        } 
-      </View>   
-    );  
+        }
+      </View>
+    );
   }
 }
- 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center', 
-    justifyContent: 'center', 
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text: {
     fontSize: 30,
--- components/navigation.js
+++ components/navigation.js
@@ -1,34 +1,72 @@
 import React, { Component } from 'react'
-import { KeyboardAvoidingView, TextInput, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image } from 'react-native';
+import { Constants, Location, Permissions, MapView } from 'expo';
+import flagBlueImg from './assets/flag-blue.png';
 
 export default class Navigation extends Component {
   constructor(props){
     super(props)
     this.state = {
-      searchInput: ""
+      region: null,
+      searchInput: "",
+      errorMessage: null,
+    }
+    this.onRegionChange = this.onRegionChange.bind(this)
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
     }
   }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let region = await Location.getCurrentPositionAsync({});
+    this.setState({ region: {
+      latitude: region.coords.latitude,
+      longitude: region.coords.longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    } });
+    console.log(region, region.coords.latitude)
+  };
+
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+
   render(){
     return(
-      <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-        <Text style={styles.title}>Wandy</Text>
-        <Text style={styles.text}>Para onde quer ir?</Text>
-        <View style={styles.inputContainer}>
-          <TextInput
-            underlineColorAndroid={'transparent'}
-            style={styles.input}
-            onChangeText={(searchInput) => {
-              this.setState({searchInput})
-              console.log(searchInput)
-            }}
-            value={this.state.searchInput}
-            placeholder="Instituto Militar de Engenharia"
-          />
-        </View>
-        <View style={styles.button}>
-          <Button title={"Navegar"} onPress={() => {console.log("Falta implementar :D")}} color="#663399"/>
-        </View>
-      </KeyboardAvoidingView>
+      <View style={styles.container}>
+          {!this.state.region && <Text>Carregando rota...</Text>}
+          {this.state.region &&
+          <View style={styles.container}>
+            <MapView
+              style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+              }}
+              region={this.state.region}
+              onRegionChangeComplete={this.onRegionChange}
+            />
+            {/* <Image source={require('./assets/flag-blue.png')} style={{width: 30, height: 30}} /> */}
+          </View>
+          }
+      </View>
     )
   }
 }
